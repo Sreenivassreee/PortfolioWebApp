ed6036d56561ad589c67525f906a48556be39121
Sreenivas K <sreenivask@Sreenivass-MacBook-Air.local>
2020-09-10 20:19:53 +0530

    Added Icons,and added Url navigation

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/Projects.dart';
@@ -34,9 +35,21 @@ class Homepage extends StatelessWidget {
                         child: CircleAvatar(
                           backgroundColor: Colors.grey[300],
                           radius: 130,
-                          child: Image.asset(
-                            'images/sree.png',
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                "https://firebasestorage.googleapis.com/v0/b/portfolio-web-33015.appspot.com/o/sree.png?alt=media&token=de277b11-4933-4f23-a837-f499a010540e",
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) =>
+                                    CupertinoActivityIndicator(
+                                        // value: downloadProgress.progress,
+                                        ),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
                           ),
+
+                          // child: Image.network(
+                          //   'https://firebasestorage.googleapis.com/v0/b/portfolio-web-33015.appspot.com/o/sree.png?alt=media&token=de277b11-4933-4f23-a837-f499a010540e',
+                          // ),
                           // shape: BoxShape.circle
                         ),
                       ),
@@ -53,12 +66,15 @@ class Homepage extends StatelessWidget {
                     ),
                     space(),
                     space(),
-                    Text(
-                      "About",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "About",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
                       ),
                     ),
                     space(),
@@ -68,13 +84,26 @@ class Homepage extends StatelessWidget {
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Social(),
+                            Social(
+                              url:
+                                  "https://firebasestorage.googleapis.com/v0/b/portfolio-web-33015.appspot.com/o/icons%2Ficonfinder_mark-github_298822.png?alt=media&token=041b0ba4-82b1-4f79-ba99-46d82836eab4",
+                              navUrl: "https://github.com/Sreenivassreee",
+                            ),
                             space(),
-                            Social(),
+                            Social(
+                              url:
+                                  "https://firebasestorage.googleapis.com/v0/b/portfolio-web-33015.appspot.com/o/icons%2Ficonfinder_Circled_Linkedin_svg_5279114(1).png?alt=media&token=6daa82d3-89ab-4a74-924c-d3facc81a729",
+                            ),
                             space(),
-                            Social(),
+                            Social(
+                              url:
+                                  "https://firebasestorage.googleapis.com/v0/b/portfolio-web-33015.appspot.com/o/icons%2F5279120-512.png?alt=media&token=f1b8d03f-ef21-46ba-839d-7efbd11775ca",
+                            ),
                             space(),
-                            Social(),
+                            Social(
+                              url:
+                                  "https://firebasestorage.googleapis.com/v0/b/portfolio-web-33015.appspot.com/o/icons%2Ficonfinder_Circled_Instagram_svg_5279112.png?alt=media&token=76092cad-3a8b-4dc4-8bc6-e4ab235b8d07",
+                            ),
                             space(),
                             Social(),
                             space(),
@@ -84,12 +113,15 @@ class Homepage extends StatelessWidget {
                       ),
                     ),
                     space(),
-                    Text(
-                      "Projects",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Projects",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
                       ),
                     ),
                     space(),
@@ -130,12 +162,12 @@ class Homepage extends StatelessWidget {
 
 Widget space() {
   return SizedBox(
-    height: 30,
+    height: 20,
   );
 }
 
 Widget space2() {
   return SizedBox(
-    height: 10,
+    height: 5,
   );
 }
--- lib/Social.dart
+++ lib/Social.dart
@@ -1,15 +1,39 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Social extends StatelessWidget {
+  Social({this.url, this.navUrl});
+  String url, navUrl;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        height: 100,
-        width: 100,
-        color: Colors.green,
+        child: GestureDetector(
+          onTap: () => _launchURL(navUrl),
+          child: CachedNetworkImage(
+            imageUrl: url ?? " ",
+            progressIndicatorBuilder: (context, url, downloadProgress) =>
+                CupertinoActivityIndicator(
+                    // value: downloadProgress.progress,
+                    ),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+        ),
+        height: 70,
+        width: 70,
       ),
     );
+  }
+}
+
+_launchURL(navUrl) async {
+  print("Pressed $navUrl`");
+  if (await canLaunch(navUrl)) {
+    await launch(navUrl);
+  } else {
+    throw 'Could not launch $navUrl';
   }
 }
